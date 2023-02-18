300374440fb1a6eda54a427c7f7914bb242c2e7f
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-02-18 12:04:02 +0600

    Formula added

--- js/function.js
+++ js/function.js
@@ -5,3 +5,20 @@ function getValueFromInput(id){
     const inputValue = parseFloat(inputValueString);
     return inputValue;
 }
+
+//set value function
+function setValueInText (id,returnValue){
+    const mainId = document.getElementById(id)
+    mainId.innerText = returnValue;
+}
+//set value in formula
+function setValueFormula (input1,input2,setValue1,setValue2){
+    const formulaA = document.getElementById(input1);
+    const formulaB = document.getElementById(input2);
+    if(isNaN(setValue1) || isNaN(setValue2)){
+        alert("Enter a valid number")
+        return;
+    }
+    formulaA.innerText = setValue1;
+    formulaB.innerText = setValue2;
+}
